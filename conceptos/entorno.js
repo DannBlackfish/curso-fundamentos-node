6c957f384c26ae79b0e4e93c76cbfdcd97c9cd56
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-18 15:29:22 -0500

    variables de entorno

---
+++ conceptos/entorno.js
@@ -0,0 +1,9 @@
+//El código no debe de guardar ninguna clave o información que constanteme vaya a cambbiar, para eso están las variables de entorno 
+//Por fuera se declara la variable
+//Podemos darle un valor por defecto 
+//Se ponen las mayúsculas son las variables de entonro que vienen desde fuera
+let nombre = process.env.NOMBRE || 'sin nobmre';
+let web = process.env.WEB || 'no tengo web';
+
+console.log('Hola' + nombre);
+console.log('Mi web es' + web);
