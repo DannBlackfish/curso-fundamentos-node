614c1a55442b28f2128f8607f8138e7cd8ffc586
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-18 20:07:45 -0500

    async/await

---
+++ async/asyncAwait.js
@@ -0,0 +1,39 @@
+async function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function() {
+            console.log('Hola, ' + nombre);
+            resolve(nombre)
+        }, 1500);
+    });
+}
+
+function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            console.log('Bla Bla Bla Bla...');
+            resolve(nombre);
+            reject('Hay un error')
+        }, 1000)
+    })
+}
+
+function adios(nombre) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(function() {
+            console.log('Adios ', nombre);
+            resolve();
+        }, 1000);
+    })
+}
+
+async function main() {
+    let nombre = await hola('Daniela')
+    await hablar();
+    await hablar();
+    await adios(nombre)
+    console.log('Termina el proceso')
+}
+
+console.log('Empezamos el proceso')
+main();
+
